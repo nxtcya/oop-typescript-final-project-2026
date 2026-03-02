1560bff3080f434effef14ef31eedb69aac9187e
nxtcya <quinn3nxt@gmail.com>
2026-03-03 02:06:28 +0700

    creatUML

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ServiceModule } from './modules/service/service.module';
+import { AppointmentModule } from './modules/appointment/appointment.module';
+import { ServiceModule } from './modules/service/service.module';
 
 @Module({
-  imports: [],
+  imports: [ServiceModule, AppointmentModule],
   controllers: [],
   providers: [],
 })
